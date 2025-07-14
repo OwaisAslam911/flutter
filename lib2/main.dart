d45d52f1176d69095c338c814c95a41416d93c7d
Owais Aslam <127122701+OwaisAslam911@users.noreply.github.com>
2025-07-14 05:32:33 -0700

    Add files via upload

---
+++ lib2/main.dart
@@ -0,0 +1,75 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:project/Screens/Admin/UsersList/customer_lists_screen.dart';
+import 'package:project/Screens/Login.dart';
+import 'package:project/Screens/SignUp.dart';
+import 'package:project/Screens/ApiProduct.dart';
+import 'package:project/Screens/UserList.dart';
+import 'package:project/Screens/splashscreen.dart';
+import 'package:provider/provider.dart';
+
+import 'Screens/Admin/AddProduct/AddProductView.dart';
+import 'Screens/Admin/AdminDashboard/AdminDashboardView.dart';
+import 'Screens/Admin/Catagory/AddCatagory.dart';
+import 'Screens/Admin/Catagory/category_management_screen.dart';
+import 'Screens/Admin/UsersList/user_info_screen.dart';
+import 'Screens/Customer/CustomerProfileUpdate/customer_profile_update_screen.dart';
+import 'Screens/Customer/Home/home_view.dart';
+import 'Screens/EmailSender.dart';
+import 'Screens/Customer/Login/LoginScreen.dart';
+
+import 'Screens/Customer/SignUp/SignUpScreen.dart';
+
+import 'firebase_options.dart';
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+
+
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => CategoryController()),
+        // Add more providers here if needed
+      ],
+      child: DevicePreview(
+        enabled: true,
+        tools: const [
+          ...DevicePreview.defaultTools,
+        ],
+        builder: (context) => MyApp(),
+      ),
+    ),
+  );
+}
+
+
+
+
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      routes: {
+        '/login': (context) => LoginScreen(),
+       '/admin' : (context) => AdminDashboard(selectedIndex: 0)
+      },
+      title: 'Book App',
+      theme: ThemeData(
+        primarySwatch: Colors.grey,
+        fontFamily: 'Inter',
+      ),
+       home:SplashScreen(),// CustomerProfileUpdateScreen(),// AdminDashboard(selectedIndex: 0,),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
